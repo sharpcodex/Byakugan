be2c1efc51836294595f5aa1ef05c9c225f0eade
sharpcodex <sharpcodex@gmail.com>
2019-10-12 16:19:06 +0300

    Create AppContext class

--- src/main/python/Byakugan/main.py
+++ src/main/python/Byakugan/main.py
@@ -1,12 +1,23 @@
 from fbs_runtime.application_context.PyQt5 import ApplicationContext
+from fbs_runtime.application_context import cached_property
 from PyQt5.QtWidgets import QMainWindow
 
 import sys
 
+class AppContext(ApplicationContext):
+    def setup():
+        pass
+
+    @cached_property
+    def main_window(self):
+        return QMainWindow()
+
+    def run(self):
+        self.main_window.resize(250, 150)
+        self.main_window.show()
+        return self.app.exec_()
+
 if __name__ == '__main__':
-    appctxt = ApplicationContext()
-    window = QMainWindow()
-    window.resize(250, 150)
-    window.show()
-    exit_code = appctxt.app.exec_()
+    app_ctxt = AppContext()
+    exit_code = app_ctxt.run()
     sys.exit(exit_code)
