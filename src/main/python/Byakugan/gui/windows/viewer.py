44af1ce427daa157d0c95ae7616837969bad5493
sharpcodex <sharpcodex@gmail.com>
2019-10-14 18:15:36 +0300

    Add passed image reading

--- src/main/python/Byakugan/gui/windows/viewer.py
+++ src/main/python/Byakugan/gui/windows/viewer.py
@@ -6,11 +6,12 @@ from gui.windows.ui.ui_viewer import Ui_ViewerWindow
 
 
 class ViewerWindow(QMainWindow, Ui_ViewerWindow):
-    def __init__(self, app_manager, theme_manager, *args, **kwargs):
+    def __init__(self, app_manager, theme_manager, images_list, *args, **kwargs):
         # class init
         super(ViewerWindow, self).__init__(*args, **kwargs)
         self.app = app_manager
         self.theme = theme_manager
+        self.images_list = images_list
         # Setup Ui
         self.setupUi(self)
         self.actions = WindowActions(self.theme)
@@ -20,17 +21,27 @@ class ViewerWindow(QMainWindow, Ui_ViewerWindow):
         self.setWindowTitle(self.app.config.app_name)
         self.setWindowIcon(self.theme.window_icon)
         self.toolbar.setToolButtonStyle(Qt.ToolButtonTextBesideIcon)
+        self.label.setScaledContents(True)
         # Setup actions
         self.actions.full_screen_action.triggered.connect(self.full_screen_action_triggered)
         self.actions.exit_action.triggered.connect(self.app.quit)
         # Setup toolbar
         self.toolbar.addAction(self.actions.full_screen_action)
         self.toolbar.addAction(self.actions.exit_action)
+        # Setup config
+        self.width = self.width()
+        self.height = self.height()
+        # Show first image
+        self.show_image(self.images_list.get_next())
 
     # Actions
     @staticmethod
     def full_screen_action_triggered():
         print("Show in full screen mode")
+
+    def show_image(self, vimage):
+        pixmap = vimage.pixmap.scaled(self.width, self.height, Qt.KeepAspectRatio, Qt.FastTransformation)
+        self.label.setPixmap(pixmap)
 
     # Helpers
     def bar_log(self, msg):
--- src/main/python/Byakugan/main.py
+++ src/main/python/Byakugan/main.py
@@ -1,16 +1,33 @@
 import sys
+import os
+from pathlib import Path
 
 from fbs_runtime.application_context.PyQt5 import ApplicationContext
 
 from app.app_manager import AppManager
 from gui.themes.theme_manager import ThemeManager
 from gui.windows.main import MainWindow
+from gui.windows.viewer import ViewerWindow
+from helpers.images_list import ImagesList
+
+TEST_VIEWER = True
 
 if __name__ == '__main__':
     app_manager = AppManager(ApplicationContext())
     theme_manager = ThemeManager(app_manager)
 
-    window = MainWindow(app_manager, theme_manager)
+    if len(sys.argv) > 1:
+        target_image = sys.argv[1]
+    elif TEST_VIEWER:
+        target_image = str(Path.home() / 'testdata' / 'test-image.jpg')
+    else:
+        target_image = ''
+
+    if os.path.isfile(target_image):
+        window = ViewerWindow(app_manager, theme_manager, ImagesList([target_image]))
+    else:
+        window = MainWindow(app_manager, theme_manager)
+
     themed_window = theme_manager.setup(window)
     themed_window.show()
 
